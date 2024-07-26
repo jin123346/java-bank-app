a71d75213db4ab4f5604264124c099b0b633f023
hajhi7899 <hajhi7899@gmail.com>
2024-07-26 12:27:10 +0900

    update java-bank-app project

--- Java/src/bank/app/BankApp.java
+++ Java/src/bank/app/BankApp.java
@@ -29,11 +29,10 @@ public class BankApp {
 				withdraw();
 			}else if(selectNo ==5) {
 				run= false;
-			}
-		
-			
+			}	
 		}
 		
+		sc.close();
 		System.out.println("프로그램 종료");
 	}
 	
@@ -47,13 +46,14 @@ public class BankApp {
 		int balance = Integer.parseInt(sc.nextLine());
 		Account account = new Account(ano,owner,balance);
 		accounts.add(account);
-		System.out.println("결과 계좌가 생성되었습니다.");
+		System.out.println("결과 : 계좌가 생성되었습니다.");
 		
 	}
 	
 	public static void accountList() {
 		System.out.println("-----------------계좌목록-----------------");
-		accounts.stream().forEach(t -> System.out.println(t.getAno() + " "+ t.getOwner()+" "+t.getBalance()));
+		accounts.stream().forEach(t -> 
+			System.out.println(t.getAno() + " "+ t.getOwner()+" "+t.getBalance()));
 		System.out.println();
 	}
 	
@@ -64,14 +64,13 @@ public class BankApp {
 		System.out.print("예금액 : ");
 		int balance = Integer.parseInt(sc.nextLine());
 		Account acc = findAccount(ano);
-		
-		int update = acc.getBalance()+balance;
-		acc.setBalance(update);
-
-		System.out.println("결과 : 예금이 성공되었습니다.");
-		
-		
-		
+		if(acc==null) {
+			System.out.println("결과 : 동일한 계좌가 없습니다.");
+		}else {
+			int update = acc.getBalance()+balance;
+			acc.setBalance(update);
+			System.out.println("결과 : 예금이 성공되었습니다.");	
+		}
 		
 	}
 	
